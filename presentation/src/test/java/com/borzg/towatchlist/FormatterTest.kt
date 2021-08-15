d8cb4138647ac1a0fc1644bf7e06eed55fa1302b
borzg <borzilov.gv@gmail.com>
2021-08-15 18:51:31 +0300

    Git actions setup

--- presentation/src/test/java/com/borzg/towatchlist/FormatterTest.kt
+++ presentation/src/test/java/com/borzg/towatchlist/FormatterTest.kt
@@ -6,6 +6,10 @@ import org.junit.Assert.*
 import org.junit.Test
 
 class FormatterTest {
+    @Test
+    fun failTest() {
+        assertEquals(0, 1)
+    }
 
     @Test
     fun numberFormatter_numberByDigitsIsCorrect() {
