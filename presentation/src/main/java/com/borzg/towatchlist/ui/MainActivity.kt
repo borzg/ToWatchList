ce03bb1cff5395047dc6f7cc3121a3a5a9c1f79d
borzg <borzilov.gv@gmail.com>
2021-08-15 20:30:36 +0300

    Check

--- presentation/src/main/java/com/borzg/towatchlist/ui/MainActivity.kt
+++ presentation/src/main/java/com/borzg/towatchlist/ui/MainActivity.kt
@@ -95,6 +95,7 @@ class MainActivity : AppCompatActivity() {
                 startDestination = Screen.BottomNavigation.WatchList.route,
                 Modifier.padding(innerPadding)
             ) {
+
                 composable(Screen.BottomNavigation.WatchList.route) {
                     WatchlistScreen(
                         navController,
